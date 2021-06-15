f32ed09ddbad4f51b4a6301e489c234b65898b7b
zeev slobo <slowvodka@gmail.com>
2021-06-15 10:02:51 +0300

    m

--- src/CS_2020_A_85.java
+++ src/CS_2020_A_85.java
@@ -86,11 +86,53 @@ public class CS_2020_A_85 {
 
 
     //Q3
+	
+	//what - how many nodes in tree
+	//something - checks all nodes between min and max values and that the tree is a binary tree
+	//secret - finds out the maximum number of nodes that represent a binary tree between min and max values.
+	
+	//3a. - 9
+	//3b. - how many nodes in tree
+	//3c. - false - since it is not a binary tree
+	//3d. - checks all nodes between min and max values and that the tree is a binary tree
+	//3e. - 4
+	//3f. - finds out the maximum number of nodes that represent a binary tree between min and max values.
 
     //Q4
+	//a
+	//	a[0] txt="good luck"
+	//	a[1] txt="special"
+	//	a[2] txt="oyvey", 		times = "2"
+	//	a[3] txt="special", 	times = "2" 
+	//	a[4] txt="submarines", 	times = "2" 	lines = "3"
+	//	a[5] txt="planes", 		times = "3" 	lines = "3"
+	
+	//b
+	/*
+	0***goodluck
+	1***special
+	1***oyveyoyvey
+	1***specialspecial
+	1***submarinessubmarines
+	submarinessubmarines
+	submarinessubmarines
+	
+	1***planesplanesplanes
+	planesplanesplanes
+	planesplanesplanes
+	
+	*/
 
     //Q5
+	//what - moves all negative numbers to the begining of the list, returns the node with the first positive number
+	//secret - makes the list in +-+-+-+- order
 
+	//5a. - 8
+	//5b. - {8,-4,2,-5,9,-6}
+	//5c. - {2,-2,3,-2,2,2}
+	//5d. - method arranges list in +-+-+- order. 
+	//if all positive or negative numbers - list wont change
+	//if many positives or many negatives - 
 
 
 }
